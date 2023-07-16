3f3bceecf7bdab659dbdfe48ee1eb7e795e43abb
tesfit.habtemichael@wsu.edu <tesfit>
2023-07-16 16:35:31 -0700

    second try

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 import random 
 #i have added coments here 
 
-app = Flask(__name__)
+app = Flask(tesfit)
 
 # list of cat images
 images = [
@@ -15,5 +15,5 @@ def index():
     url = random.choice(images)
     return render_template('index.html', url=url)
 
-if __name__ == "__main__":
+if tesfit == "__main__":
     app.run(host="0.0.0.0")
